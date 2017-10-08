b82652db915966cd4fd7638fef477b3253c26ab2
Snoople <cabloblo@hotmail.com>
2017-10-08 16:30:25 +1100

    chat app working

---
+++ ReminderApp/app/src/main/java/com/example/r2m/reminderapp/MyFirebaseInstanceIDService.java
@@ -0,0 +1,27 @@
+package com.example.r2m.reminderapp;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by Snoople on 10/8/2017.
+ */
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        // TODO: sendRegistrationToServer(refreshedToken);
+    }
+
+}
---
+++ ReminderApp/app/src/main/java/com/example/r2m/reminderapp/UserDetails.java
@@ -0,0 +1,11 @@
+package com.example.r2m.reminderapp;
+
+/**
+ * Created by Snoople on 10/8/2017.
+ */
+
+public class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
