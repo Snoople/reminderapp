a0c51d827e2a061ec513324ceea9f8472c42035f
Snoople <cabloblo@hotmail.com>
2017-10-16 22:33:10 +1100

    loltest2

--- ReminderApp/app/src/main/java/com/example/r2m/reminderapp/MainActivity.java
+++ ReminderApp/app/src/main/java/com/example/r2m/reminderapp/MainActivity.java
@@ -11,7 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         startActivity(new Intent(MainActivity.this, Login.class));
-        //test
     }
 }
 
