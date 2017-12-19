7bf003f4269f389a7970ac6120b465ac7e05acec
iwasunowimme <josh.wright92@hotmail.com>
2017-12-19 22:56:38 +1100

    buttons overlay

--- ReminderApp/app/src/main/java/com/example/r2m/reminderapp/Reminder.java
+++ ReminderApp/app/src/main/java/com/example/r2m/reminderapp/Reminder.java
@@ -2,12 +2,44 @@ package com.example.r2m.reminderapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TimePicker;
 
 public class Reminder extends AppCompatActivity {
+
+    TimePicker editTime;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminder);
+
+        editTime = (TimePicker)findViewById(R.id.editTime);
+
+
+//        editDate.setVisibility(View.VISIBLE);
+
     }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case R.id.action_settings:
+//                // User chose the "Settings" item, show the app settings UI...
+//                return true;
+//
+//            case R.id.action_favorite:
+//                // User chose the "Favorite" action, mark the current item
+//                // as a favorite...
+//                return true;
+//
+//            default:
+//                // If we got here, the user's action was not recognized.
+//                // Invoke the superclass to handle it.
+//                return super.onOptionsItemSelected(item);
+//
+//        }
+//    }
 }
